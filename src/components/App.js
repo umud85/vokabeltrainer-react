f826012a667b6d10e55bf0a60124d1e8867ef8d3
umud85 <akbayinu@googlemail.com>
2022-03-13 08:42:06 +0100

    Map approach

--- src/components/App.js
+++ src/components/App.js
@@ -15,55 +15,45 @@ export default function App() {
   const [german, setGerman] = useState([]);
 
   useEffect(() => {
-    let loadingData = true;
-    if (loadingData) {
-      const getData = async () => {
-        const response = await fetch(Vokabeln);
-        const rawData = await response.text();
-        const formattedData = rawData.split("\n").splice(1);
-        const learningVocs = choseLearningVocs(formattedData);
-        setData(learningVocs)
-      }
-      getData();
+    const getData = async () => {
+      const response = await fetch(Vokabeln);
+      const rawData = await response.text();
+      const formattedData = rawData.split("\n").splice(1);
+      const learningVocs = choseLearningVocs(formattedData);
+      setData(learningVocs)
     }
-    return () => {
-      loadingData = false;
-    }
+    getData();
   }, []);
 
   const choseLearningVocs = (vocList) => {
-    const learningVocs = [];
-    while (learningVocs.length < 5) {
+    const learningVocs = new Map();
+    while (learningVocs.size < 5) {
       let randomNumber = Math.floor(Math.random() * vocList.length);
-      if (!learningVocs.includes(vocList[randomNumber])) {
-        learningVocs.push(vocList[randomNumber]);
+      if (!learningVocs.has(vocList[randomNumber].split(",")[0])) {
+        let trimmedPair = vocList[randomNumber].trim();
+        learningVocs.set(trimmedPair.split(",")[0], trimmedPair.split(",")[1]);
       }
     }
     return learningVocs;
   } 
 
+  const nextVoc = () => {}
+
   const sourceVocs = (chosenVocs) => {
-    const sourceV = [chosenVocs.map(voc => {
-      return voc.split(",")[0];
-    })]
-    return sourceV;
+    console.log(chosenVocs.keys())
   };
 
   const targetVocs = (chosenVocs) => {
-    const targetV = [chosenVocs.map(voc => {
-      return voc.split(",")[1];
-    })]
-    return targetV;
+   console.log(chosenVocs)
   };
 
   const startGame = () => {
     setGameStarted(true);
     setEnglish(sourceVocs(data));
-    console.log(english);
-
   }
 
   return <Fragment>
+    {console.log(data)}
     <div className="container">
       <div className="box-title">
       <h2>Vokabeln Englisch / Deutsch</h2>
@@ -75,7 +65,6 @@ export default function App() {
         gameStarted ? <Answer currentV={currentVoc} message={"OK"} /> :
         <Answer handleClick={startGame} currentV={currentVoc} message={"Start"} /> 
       }
-      {console.log(english)}
     </div>
   </Fragment>
 }
