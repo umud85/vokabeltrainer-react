4868e025db752d9e9472ddff7a34a43721b2e67f
umud85 <akbayinu@googlemail.com>
2022-03-23 18:02:16 +0100

    Before adding new functionality

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -19,8 +19,30 @@ const Answer = ({ status, message, handleClick, handleChange, value }) => {
         type="text"
         name="answer"
         value=""
-      />
-      <div><input type="submit" id="my-button" value="Start again" /></div>
+        />
+        <div className="button-dropdown-wrapper">
+          <label htmlFor="numberVocs">Vokabelanzahl:
+            <select name="numberVocs" id="numberVocs">
+              <option value="five">5</option>
+              <option value="six">6</option>
+              <option value="seven" selected>7</option>
+              <option value="eight">8</option>
+              <option value="nine">9</option>
+              <option value="ten">10</option>
+              <option value="eleven">11</option>
+              <option value="twelve">12</option>
+              <option value="thirteen">13</option>
+              <option value="fourteen">14</option>
+              <option value="fifteen">15</option>
+              <option value="sixteen">16</option>
+              <option value="seventeen">17</option>
+              <option value="eighteen">18</option>
+              <option value="nineteen">19</option>
+              <option value="twenty">20</option>
+            </select>
+          </label>
+          <input type="submit" id="my-button" value="Start again" />
+        </div>
     </div>
   </form>
   }
@@ -36,11 +58,34 @@ const Answer = ({ status, message, handleClick, handleChange, value }) => {
         value={value}
         onChange={handleChange}
         ref={inputElement}
-      />
-      {message === "Start" ?
-        <div><input type="submit" id="my-button" value={message} /></div> :
-        <div><input type="submit" id="my-button" value={message} /></div>
-      }
+        />
+        <div className="button-dropdown-wrapper">
+          <label htmlFor="numberVocs">Vokabelanzahl:
+            <select name="numberVocs" id="numberVocs">
+              <option value="five">5</option>
+              <option value="six">6</option>
+              <option value="seven" selected>7</option>
+              <option value="eight">8</option>
+              <option value="nine">9</option>
+              <option value="ten">10</option>
+              <option value="eleven">11</option>
+              <option value="twelve">12</option>
+              <option value="thirteen">13</option>
+              <option value="fourteen">14</option>
+              <option value="fifteen">15</option>
+              <option value="sixteen">16</option>
+              <option value="seventeen">17</option>
+              <option value="eighteen">18</option>
+              <option value="nineteen">19</option>
+              <option value="twenty">20</option>
+            </select>
+          </label>
+          {message === "Start" ?
+            <div><input type="submit" id="my-button" value={message} /></div> :
+            <div><input type="submit" id="my-button" value={message} /></div>
+          }
+        </div>
+        
     </div>
   </form>
   }
